597e720d0e095d8a3f12331b26b10a3c86c69280
Rodrigo Rato <rodrigo.rato@rnl.tecnico.ulisboa.pt>
2017-11-06 15:59:42 +0000

    Adds server module

---
+++ server/src/main/java/a16/yarfs/server/App.java
@@ -0,0 +1,13 @@
+package a16.yarfs.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

1d4e2969ad53ad246e70b24624683e81df0bc5cd
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2017-11-13 17:50:33 +0000

    Adds a FileAccessException

---
+++ server/src/main/java/a16/yarfs/server/exception/FileAccessException.java
@@ -0,0 +1,10 @@
+package a16.yarfs.server.exception;
+
+/**
+ * Created by Rodrigo Rato on 11/13/17.
+ */
+public class FileAccessException extends YarfsServerException {
+    public FileAccessException(String s) {
+        super(s);
+    }
+}

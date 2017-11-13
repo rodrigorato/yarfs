c78e6d2ffdcf04595e26bf004142b0b5cff76eac
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2017-11-13 15:47:58 +0000

    Adds FileManager interface

--- server/src/main/java/a16/yarfs/server/domain/FileManager.java
+++ server/src/main/java/a16/yarfs/server/domain/FileManager.java
@@ -1,6 +1,32 @@
 package a16.yarfs.server.domain;
 
+/**
+ * This interface defines what operations a FileManager should have.
+ *
+ * A FileManager is an abstraction (something
+ * along the lines of a data-mapper) that allows
+ * us to store/retrieve values on a disk/databases/whatever.
+ */
 public interface FileManager {
+    /**
+     * This is the method that should be called
+     * to store a File somewhere.
+     *
+     * A possible implementation would be to
+     * serialize this file.
+     * @param file is the file to be stored
+     */
+    void writeFile(File file);
 
+    /**
+     * This is the method that should be called
+     * to retrieve a file that is in storage.
+     *
+     * A possible implementation would be to
+     * un-serialize a file from the disk.
+     * @param fileId is an unique ID that represents the file.
+     * @return the File that was once stored.
+     */
+    File readFile(long fileId);
 }
 

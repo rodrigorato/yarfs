9949c93f2c2723b47667a9436dd6bd3a727eaa00
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2017-12-03 04:28:20 +0000

    client: adds App argument support

--- client/src/main/java/a16/yarfs/client/App.java
+++ client/src/main/java/a16/yarfs/client/App.java
@@ -1,6 +1,7 @@
 package a16.yarfs.client;
 
 import a16.yarfs.client.presentation.YarfsShell;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 
@@ -10,8 +11,14 @@ import java.io.IOException;
  */
 public class App 
 {
+    private static final Logger logger = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
+        if(!parseArgs(args)) {
+            System.out.print(getUsage());
+            return;
+        }
         System.out.println( "Starting yarfs client..." );
         YarfsShell shell = new YarfsShell(System.in, System.out, true);
         try {
@@ -20,4 +27,28 @@ public class App
             e.printStackTrace();
         }
     }
+
+    private static boolean parseArgs(String[] args) {
+        for ( String arg : args) {
+            if(arg.equals("--help")){
+                System.out.print(getUsage());
+                System.exit(0);
+            } else {
+                logger.error("unknown argument: " + arg);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String getUsage() {
+        String EOL = System.lineSeparator();
+        String TAB = "\t";
+        return "Usage: " + App.class.getSimpleName() + " [OPTIONS] [BASEURL]" + EOL +
+                EOL +
+                "OPTIONS:" + EOL +
+                TAB + "--help      " + TAB + "print usage information and exit" + EOL +
+                TAB + EOL;
+    }
+
 }

d34be47289dabf06566cbc6c9ce9ddff0d919874
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2017-12-04 18:30:42 +0000

    Adds CA support for custom server URL

--- ca/src/main/java/a16/yarfs/ca/App.java
+++ ca/src/main/java/a16/yarfs/ca/App.java
@@ -1,13 +1,17 @@
 package a16.yarfs.ca;
 
+import a16.yarfs.ca.service.AuthenticateService;
+import a16.yarfs.ca.service.CAConstants;
 import org.apache.log4j.Logger;
+
+import java.io.IOException;
 
 
 /**
  * The entry point for our yarfs ca
  *
  */
-public class App 
+public class App
 {
     private static final Logger logger = Logger.getLogger(App.class);
 
@@ -17,7 +21,8 @@ public class App
             System.out.print(getUsage());
             return;
         }
-        System.out.println( "Starting yarfs ca..." );
+        System.out.println( "Starting yarfs CA..." );
+        
     }
 
     private static boolean parseArgs(String[] args) {
@@ -25,6 +30,9 @@ public class App
             if(arg.equals("--help")){
                 System.out.print(getUsage());
                 System.exit(0);
+            } else if(arg.startsWith("http")) {
+                logger.info("Using base URL " + arg);
+                CAConstants.baseUrl = arg;
             } else {
                 logger.error("unknown argument: " + arg);
                 return false;
@@ -36,6 +44,14 @@ public class App
     public static String getUsage() {
         String EOL = System.lineSeparator();
         String TAB = "\t";
-        return "Usage: " + App.class.getSimpleName();
+        return "Usage: " + App.class.getSimpleName() + " [OPTIONS] [BASEURL]" + EOL +
+                EOL +
+                TAB + "BASEURL     " + TAB + "base URL for the yarfs server (default: " + CAConstants.baseUrl + ")" + EOL +
+                EOL +
+                "OPTIONS:" + EOL +
+                TAB + "--help      " + TAB + "print usage information and exit" + EOL +
+                TAB + EOL;
     }
+
 }
+

f6234d4ac5ccf117b52206d05f81dda86f372fcb
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2017-12-07 16:04:13 +0000

    Fixes runclient indentation

--- client/runclient.sh
+++ client/runclient.sh
@@ -1,7 +1,8 @@
 mkdir my-repo
 pushd .
 cd ../ca
-mvn install:install-file -Dfile=target/ca-1.0-SNAPSHOT.jar                          -DgroupId=a16                        -DartifactId=ca                          -Dversion=1.0-SNAPSHOT                          -Dpackaging=jar                          -DlocalRepositoryPath=../client/my-repo
+mvn compile package
+mvn install:install-file -Dfile=target/ca-1.0-SNAPSHOT.jar -DgroupId=a16 -DartifactId=ca -Dversion=1.0-SNAPSHOT -Dpackaging=jar -DlocalRepositoryPath=../client/my-repo
 popd
 mvn dependency:purge-local-repository
 mvn -U compile exec:java -Dexec.args="https://server.yarfs:31000"
